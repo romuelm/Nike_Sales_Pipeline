48c069f83689b1f4c37a0b9f0707b191bceb3d4d
romuelm <11romuelmasilang@gmail.com>
2026-05-31 14:10:56 +0800

    chore: save requirements and current progress

--- etl.py
+++ etl.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sqlalchemy import create_engine
 # 1. Load Data
 df = pd.read_csv('Nike_Sales_Uncleaned.csv')
 print(f"Original Shape: {df.shape}")
@@ -16,5 +16,11 @@ df['Region'] = df['Region'].replace(region_mapping)
 # 3.Handling Missing Values for Units_Sold Column
 df['Units_Sold'] = df['Units_Sold'].fillna(1)
 
-# 4. Save Cleaned Data to a new file
-df.to_csv('Nike_Sales_Cleaned.csv', index=False)
+# 4. Connecting to PostgreSQL and Upload
+print("Connecting to database...")
+# Format: postgresql://user:password@host:port/database_name
+engine = create_engine('postgresql://admin:password@localhost:5432/nike_sales')
+
+# Write the dataframe to a SQL table named 'sales_data'
+df.to_sql('sales_data', engine, if_exists='replace', index=False)
+print("Success! Data loaded into PostgreSQL.")
